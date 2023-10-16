90bd21b49de682bc44b57edeb5a9a2467ccc3f87
sh15935 <safazsahih@gmail.com>
2023-10-16 22:49:02 +0000

    tasks

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -5,6 +5,20 @@ Tests for the Place Model
 
 
 import unittest
-import datetime
-
 from models.place import Place
+
+class TestPlace(unittest.TestCase):
+    def test_place_initialization(self):
+        place = Place()
+        self.assertIsInstance(place, Place)
+
+    def test_place_attributes(self):
+        place = Place()
+        # Set attributes and check if they are properly set
+
+    def test_place_methods(self):
+        place = Place()
+        # Call methods and assert the expected outcomes
+
+if __name__ == '__main__':
+    unittest.main()
